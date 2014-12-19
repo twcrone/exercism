4f0df6e5d0b8d28ac5499cf93666ee857e1aaae3
Todd Crone <todd.crone@perceptivesoftware.com>
2014-12-18 22:52:29 -0500

    Fix a few cases for responding to questions

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,5 +1,10 @@
 (ns bob)
 
+(defn question? [comment]
+  (.endsWith comment "?"))
+
 (defn response-for [comment]
-  (println comment)
-  "Whatever.")
+  (if (question? comment)
+    ("Sure.")
+    "Whatever."))
+
