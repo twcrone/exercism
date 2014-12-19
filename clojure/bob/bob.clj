ae74fa08dae5447edc4b813e208f3cf3127a741d
Todd Crone <todd.crone@perceptivesoftware.com>
2014-12-18 23:09:00 -0500

    Fix a few more tests for questions and exclamations

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -6,9 +6,13 @@
 (defn exclamation? [comment]
   (.endsWith (str comment) "!"))
 
-(defn response-for [comment]
-  (if (question? comment)
-    (str "Sure.")
-    (if (exclamation? comment) (str "Whoa, chill out!")
-      "Whatever.")))
+(defn silence? [comment]
+  (= (str comment) ""))
+
+(defn response-for [initial-comment]
+  (let [comment (.trim initial-comment)]
+    (if (question? comment) (str "Sure.")
+      (if (exclamation? comment) (str "Whoa, chill out!")
+        (if (silence? comment) (str "Fine. Be that way!")
+        "Whatever.")))))
 
